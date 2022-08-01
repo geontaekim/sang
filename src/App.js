1a534ee100a9e02347cf27e161a53033cdbfff61
geontae kim <wleek2@gmail.com>
2022-08-01 17:02:47 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Create(props) {
       const body = event.target.body.value;       //form태그 안의 value들고오는 식 = event.target.name.value
       props.onCreate(title, body);
 
-    }}  >
+    }}>
       <p><input type='text' name="title" placeholder='title' /></p>
       <p><textarea name='body' placeholder='body'></textarea></p>
       <p><input type='submit' value="create"></input></p>
@@ -45,8 +45,8 @@ function Nav(props) {
       <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
         event.preventDefault();
         let id = Number(event.target.id);
-        props.onChangeMode(id);
-        // target = 이벤트를 유발시킨 태그를 가르킴
+        props.onChangeMode(id); 
+        // target = 이벤트를 유발시킨태그를 가르킴
       }}>{t.title}</a>
     </li>)
   }
@@ -99,7 +99,7 @@ function App() {
   let contextControl = null;
   console.log('mode?', mode)
   if (mode === 'WELCOME') {
-    content = <Article title="welcome" body="hello, 웰컴이유~"></Article>
+    content = <Article title="welcome" body="hello, 웰컴이유~"></Article> 
   } else if (mode === 'READ') {
     let title, body = null;
     for (let i = 0; i < topics.length; i++) {
@@ -109,12 +109,23 @@ function App() {
       }
     }
     console.log('tile?', title)
-    console.log('body?', body)
+    console.log('body?', body)                  
     content = <Article title={title} body={body}></Article>
-    contextControl = <li><a href={"/update/" + id} onClick={event => {
+    contextControl = <><li><a href={"/update/" + id} onClick={event => {
       event.preventDefault();
       setMode('UPDATE');
     }}>update</a></li>
+    <li><input type='button' value='Delete' onClick={()=>{
+      const newTopics=[];
+      for(let i=0; i<topics.length; i++){
+        if(topics[i].id!==id){
+          newTopics.push(topics[i]);
+        }
+      }
+      setTopics(newTopics);
+      setMode('WELCOME');
+    }} /></li>
+    </>  //복수의태그를 포함하는 태그 ->  '<>' 빈태그
   } else if (mode === 'CREATE') {
     
     content = <Create onCreate={(_title, _body) => {
@@ -160,7 +171,6 @@ function App() {
       <Nav topics={topics} onChangeMode={(_id) => {
         setMode("READ");
         setId(_id);
-
       }}></Nav>
       {content}
       <ul>
@@ -168,7 +178,7 @@ function App() {
           event.preventDefault();
           setMode('CREATE');
         }}>create</a></li>
-        {contextControl}
+        {contextControl}   
       </ul>
 
     </div>
